93aa44ec8d73074e82e24947885ab8a393ec8c0b
TangJo <tanguy.jooris@hotmail.com>
2022-06-25 17:08:39 +0200

    add poetry & make it work with nox

--- noxfile.py
+++ noxfile.py
@@ -5,5 +5,25 @@ python_versions = ["3.9", "3.10"]
 
 @nox.session(python=python_versions)
 def tests(session):
-    session.run("coverage", "run", "-m", "unittest", "discover", "-v", "tests", external = True) # we run coverage from external env
-    session.run("coverage", "report", "-m", "--omit=*/tests/*", external = True)
+    session.install("coverage[toml]")
+    session.run(
+        "poetry",
+        "export",
+        "-f",
+        "requirements.txt",
+        "--output",
+        "requirements.txt",
+        external=True,
+    )
+    session.install("-r", "requirements.txt")
+    session.run(
+        "coverage",
+        "run",
+        "-m",
+        "unittest",
+        "discover",
+        "-v",
+        "tests",
+        external=True,
+    )  # we run coverage from external env
+    session.run("coverage", "report", "-m", "--omit=*/tests/*", external=True)
