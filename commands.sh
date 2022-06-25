e3b8670db1b1dd2a34ce8aff3319fe666ae799af
TangJo <tanguy.jooris@hotmail.com>
2022-06-25 21:14:11 +0200

    tests11

--- commands.sh
+++ commands.sh
@@ -10,7 +10,7 @@ dev_setup(){
     pip install nox==2022.1.7
     echo "Downloading Poetry"
     curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-    . $HOME/.poetry/env
+    source $HOME/.poetry/env
     poetry install
 }
 
